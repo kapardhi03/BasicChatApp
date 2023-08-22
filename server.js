b73585b5589d79258abd9498655f90fc22dfa4e0
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-08-22 18:50:55 +0530

    Emoji Search

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(express.static(path.join(__dirname, '')));
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (msgData) => {
+        const msg = modifyMessage(msgData.message);
+        const modifiedMsgData = { ...msgData, message: msg };
+        io.emit('chat message', modifiedMsgData);
     });
 
     socket.on('disconnect', () => {
@@ -25,3 +27,20 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+function modifyMessage(message) {
+    if (message.includes('react')) {
+        return message.replace('react', '⚛️'); // React emoji
+    } else if (message.includes('hey')) {
+        return message.replace('hey', '👋'); // Wave emoji
+    } else if (message.includes('woah')) {
+        return message.replace('woah', '🤯'); // Wosh emoji
+    } else if (message.includes('lol')) {
+        return message.replace('lol', '😂'); // Laughing emoji
+    } else if (message.includes('like')) {
+        return message.replace('like', '❤️'); // Love emoji
+    } else if (message.includes('congratulations')) {
+        return message.replace('congratulations', '🎉'); // Party emoji
+    }
+    return message;
+}
+
