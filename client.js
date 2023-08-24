e77a4b24ef655f85df235a94d2496352ed24343a
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-08-24 23:23:07 +0530

    updated

--- client.js
+++ client.js
@@ -5,6 +5,21 @@ $(document).ready(() => {
     const messages = $('#messages');
     const usernameInput = $('#username');
     const roomSelect = $('#room');
+    const typingIndicator = $('#typingIndicator');
+    const sendButton = $('#sendButton');
+    $('#sendButton').hide();
+
+    let isTyping = false;
+
+    input.on('input', () => {
+        isTyping = input.val().length > 0;
+        updateTypingIndicator();
+        if (isTyping) {
+            $('#sendButton').show();
+        } else {
+            $('#sendButton').hide();
+        }
+    });
 
     form.submit((e) => {
         e.preventDefault();
@@ -16,11 +31,19 @@ $(document).ready(() => {
             };
             socket.emit('chat message', messageData);
             input.val('');
+            isTyping = false;
+            updateTypingIndicator();
         }
     });
 
     socket.on('chat message', (data) => {
         const message = `<p><strong>${data.username}:</strong> ${data.message}</p>`;
         messages.append(message);
+        isTyping = false;
+        updateTypingIndicator();
     });
+
+    function updateTypingIndicator() {
+        typingIndicator.text(isTyping ? 'Typing...' : '');
+    }
 });
